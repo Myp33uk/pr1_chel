6fe2257fd3539c388ee0f7052096adb6aa62c0f8
исп-31 <-31@LAB17-13>
2021-11-26 14:51:55 +0300

    Добавьте файлы проекта.

---
+++ Lib_14/Practic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lib_14
+{
+    public class Practic
+    {
+        /// <summary>
+        /// Вычисляет сумму целых случайных чисел, распределенных в диапазоне от 55 до 70 ,пока эта сумма не превышает некоторого числа K
+        /// </summary>
+        /// <param name="userNumber">число которое не должно быть превышено</param>
+        /// <param name="numberList">сгенерированые числа в строковой форме "1 2 3 4 ..."</param>
+        /// <param name="counterNumbers">количество сгенеррованых чисел</param>
+        /// <param name="sum">сумма сгенерированых чисел</param>
+        public static void SumNumbers(int userNumber, out int counterNumbers, out int sum, out string numberList)
+        {
+            numberList = "";
+            sum = 0;           
+            counterNumbers = 0;
+            int newNumber;
+            Random randomNumber = new Random();
+            while (sum < userNumber)
+            {
+                newNumber = randomNumber.Next(55, 70);//Рандомные числа
+                if ((sum + newNumber) < userNumber)
+                {
+                    sum += newNumber;//Вычисление суммы
+                    counterNumbers++;//Счетчик
+                    numberList += Convert.ToString(newNumber) + " ";//Заполнение строки числа
+                }
+                else break;
+            }
+        }
+    }
+}
